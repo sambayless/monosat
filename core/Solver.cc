33fa2c70a4eb2d1d04e3b351297a1a1e1d3a8c77
Sam <sam@devmint.(none)>
2013-05-28 13:01:13 -0700

    simplified a bit

--- core/Solver.cc
+++ core/Solver.cc
@@ -730,35 +730,7 @@ CRef Solver::propagate(bool propagate_theories)
 
 				if(!theories[i]->solve(theory_conflict)){
 
-					if(theory_conflict.size()==0){
-						ok=false;
-						cancelUntil(0);
-						return CRef_Undef;
-					}else if(theory_conflict.size()==1){
-						cancelUntil(0);
-						assert(var(theory_conflict[i])<nVars());
-						if(!enqueue(theory_conflict[0])){
-							ok=false;
-							return CRef_Undef;
-						}
-					}else{
-						//find the highest level in the conflict (should be the current decision level, but we won't require that)
-						int max_lev = 0;
-						for(int j = 0;j<theory_conflict.size();j++){
-							assert(var(theory_conflict[i])<nVars());
-							assert(value(theory_conflict[i])==l_False);
-							int l = level(var(theory_conflict[i]));
-							if(l>max_lev){
-								max_lev=l;
-							}
-						}
-						assert(max_lev>0);
-						cancelUntil(max_lev);
-						CRef cr = ca.alloc(theory_conflict, true);
-						clauses.push(cr);
-						attachClause(cr);
-						return cr;
-					}
+					addConflictClause(theory_conflict);
 				}
 			}
 
@@ -1023,7 +995,7 @@ lbool Solver::search(int nof_conflicts)
 
                 if (next == lit_Undef){
 
-                /*	//solve theories if this solver is completely assigned
+                	/*//solve theories if this solver is completely assigned
 					for(int i = 0;i<theories.size();i++){
 						if(opt_subsearch==3 && track_min_level<initial_level)
 							continue;//Disable attempting to solve sub-solvers if we've backtracked past the super solver's decision level
