44a1b5e487ca2b1c6856731a1c242f036226899a
Sam <sam@devmint.(none)>
2013-05-28 18:56:20 -0700

    added back in the graph classes

---
+++ graph/Graph.h
@@ -0,0 +1,41 @@
+/*
+ * Graph.h
+ *
+ *  Created on: 2013-04-13
+ *      Author: sam
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include "mtl/Vec.h"
+#include "mtl/Heap.h"
+#include "mtl/Alg.h"
+
+#include "core/SolverTypes.h"
+
+
+namespace Minisat {
+
+
+class Graph {
+public:
+	static const int Undef_Node =-1;
+
+
+public:
+
+    virtual ~Graph(){};
+
+
+public:
+	virtual int newNode()=0;
+	virtual int nNodes()=0;
+	virtual bool isNode(int n)=0;
+	virtual Lit newEdge(int from,int to)=0;
+
+	virtual void reachesAny(int from, vec<Lit> & properties_out,int within_steps)=0;
+};
+};
+
+#endif /* GRAPH_H_ */
