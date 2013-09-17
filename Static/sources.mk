b94229fe79825f50ab00d17b11be106b91207abc
sbayless <sambayless@gmail.com>
2013-09-17 16:25:59 -0700

    added mkfiles

---
+++ Static/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+CPP_SRCS := 
+C_UPPER_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+CC_SRCS := 
+OBJS := 
+C++_DEPS := 
+C_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+EXECUTABLES := 
+CXX_DEPS := 
+C_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+utils \
+core \
+aiger \
+Release2/utils \
+Release2/minilib \
+Release2/dbg \
+Release2/core \
+Release2/aiger \
+Release2 \
+. \
+
