f3f6fa78abaec3556bc2dc4759ffd3fc99f7cefa
sbayless <sambayless@gmail.com>
2015-07-12 14:11:08 -0700

    added missing makefiles

---
+++ SharedLibraryDbg/core/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../core/Config.cpp 
+
+CC_SRCS += \
+../core/Solver.cc 
+
+OBJS += \
+./core/Config.o \
+./core/Solver.o 
+
+CC_DEPS += \
+./core/Solver.d 
+
+CPP_DEPS += \
+./core/Config.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+core/%.o: ../core/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -DNDEBUG -I.././ -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -Wno-unused-variable -Wno-unused-but-set-variable  -static  -static-libgcc     -static-libstdc++ -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+core/%.o: ../core/%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -DNDEBUG -I.././ -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -Wno-unused-variable -Wno-unused-but-set-variable  -static  -static-libgcc     -static-libstdc++ -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ SharedLibraryDbg/dgl/alg/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../dgl/alg/DisjointSets.cpp \
+../dgl/alg/LinkCutCost.cpp 
+
+OBJS += \
+./dgl/alg/DisjointSets.o \
+./dgl/alg/LinkCutCost.o 
+
+CPP_DEPS += \
+./dgl/alg/DisjointSets.d \
+./dgl/alg/LinkCutCost.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+dgl/alg/%.o: ../dgl/alg/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS -DNDEBUG -I.././ -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -Wno-unused-variable -Wno-unused-but-set-variable  -static  -static-libgcc     -static-libstdc++ -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
