42a3e2bce4dbbd4f2390a96091de8f93cc387e74
sbayless <sambayless@gmail.com>
2015-07-12 14:08:34 -0700

    added setup script for python library

---
+++ api/python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='MonoSAT Python Interface',
+      author='Sam Bayless',
+      author_email='sbayless@cs.ubc.ca',
+      url='http://www.cs.ubc.ca/labs/isd/Projects/monosat/',
+      packages=['monosat'],
+     )
