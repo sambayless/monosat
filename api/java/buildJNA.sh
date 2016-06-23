cad1e63d2c2d40e2f8b796e489f06a1c5cbb1154
sbayless <sambayless@gmail.com>
2016-06-23 14:10:52 -0700

    moved code to src/monosat

--- api/java/buildJNA.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#Use to build no-frills Java interface to MonoSAT's C API
-#tested with jnaerator 0.9.4 and JNA 3.4
-cp ../../libmonosat.so .
-java -jar jnaerator.jar -DJNA=1  -direct -nocpp  -library  monosat ../Monosat.h
-mv ../Monosat.jar .
-
