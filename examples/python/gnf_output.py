8ebef64b1b0f6df22e39b3c02f4d807f627deb2b
sbayless <sambayless@gmail.com>
2017-09-15 11:51:55 -0700

    Added example demonstrating GNF file format saving and loading from python

---
+++ examples/python/gnf_output.py
@@ -0,0 +1,25 @@
+# Demonstrate writing GNF files
+
+from monosat import *
+Monosat().setOutputFile("tutorial.gnf")
+a = Var()
+b = Var()
+c = Or(a, Not(b))
+
+Assert(c)
+Assert(Not(c))
+
+#trivial contradition, so result should be false
+result = Solve()
+print(result)
+
+Monosat().init("") #clear and reinitialize the solver
+#solver now has no constraints, so it should be sat
+print(Solve())
+
+# read in the previously saved constraints (these can also be read in on the command line, eg ./monosat tutorial.gnf
+Monosat().readGNF("tutorial.gnf")
+
+result2 = Solve()
+print(result2)
+assert(result==result2)
