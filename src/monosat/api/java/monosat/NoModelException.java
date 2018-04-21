fd701abe39b40d46fc3c0577470dae8454362c7e
sbayless <sambayless@gmail.com>
2018-04-20 21:34:13 -0700

    NoModelException now extends java.util.NoSuchElementException, to allow common handling with Optional.get()

--- src/monosat/api/java/monosat/NoModelException.java
+++ src/monosat/api/java/monosat/NoModelException.java
@@ -1,10 +1,11 @@
 package monosat;
 
+import java.util.NoSuchElementException;
 /**
  * Unchecked runtime exception thrown when attempting to access the assignment of
  * a literal, bitvector, or graph property that does not have an assignment.
  */
-public class NoModelException extends RuntimeException{
+public class NoModelException extends NoSuchElementException {
     public NoModelException(String message) {
         super(message);
     }
