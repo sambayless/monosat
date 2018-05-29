b596a13bfea7137edd6af0525921dd5e5e6ccf96
sbayless <sambayless@gmail.com>
2018-05-28 18:11:37 -0700

    removed brew install commands from travis yml

--- src/monosat/api/java/tests/monosat/SolverTest.java
+++ src/monosat/api/java/tests/monosat/SolverTest.java
@@ -34,32 +34,32 @@ import static org.junit.Assert.assertTrue;
 public class SolverTest {
     @Test
     public void testSolve() {
-        Solver s = new Solver();
+        monosat.Solver s = new monosat.Solver();
         assertTrue(s.solve());
         assertTrue(s.solve());
-        assertTrue(s.solve(Lit.True));
-        assertFalse(s.solve(Lit.False));
-        assertFalse(s.solve(Lit.True, Lit.False));
+        assertTrue(s.solve(monosat.Lit.True));
+        assertFalse(s.solve(monosat.Lit.False));
+        assertFalse(s.solve(monosat.Lit.True, monosat.Lit.False));
         assertTrue(s.solve());
         System.out.println("Done");
     }
     @Test
     public void testArguments() {        
-        Solver s = new Solver("-no-reach-underapprox-cnf");
+        monosat.Solver s = new monosat.Solver("-no-reach-underapprox-cnf");
         assertTrue(s.solve());
         assertTrue(s.solve());
-        assertTrue(s.solve(Lit.True));
-        assertFalse(s.solve(Lit.False));
-        assertFalse(s.solve(Lit.True, Lit.False));
+        assertTrue(s.solve(monosat.Lit.True));
+        assertFalse(s.solve(monosat.Lit.False));
+        assertFalse(s.solve(monosat.Lit.True, monosat.Lit.False));
         assertTrue(s.solve());
         System.out.println("Done");
     }
     @Test
     public void testMultipleSolvers() {
-        Solver s = new Solver();
+        monosat.Solver s = new monosat.Solver();
         assertTrue(s.solve());
 
-        Solver s2 = new Solver();
+        monosat.Solver s2 = new monosat.Solver();
         assertTrue(s2.solve());
         assertTrue(s.solve());
         s.close();
@@ -69,13 +69,13 @@ public class SolverTest {
 
     @Test
     public void testOrUnsat(){
-        Solver s = new Solver();
+        monosat.Solver s = new monosat.Solver();
         assertTrue(s.solve());
         s.assertAnd();
         assertTrue(s.solve());
         s.assertOr();
         assertFalse(s.solve());
-        Solver s2 = new Solver();
+        monosat.Solver s2 = new monosat.Solver();
         assertTrue(s2.solve());
         assertFalse(s.solve());
     }
@@ -83,32 +83,32 @@ public class SolverTest {
 
     @Test
     public void testAddClause(){
-        Solver s = new Solver();
+        monosat.Solver s = new monosat.Solver();
         assertTrue(s.solve());
         s.addClause();
         assertFalse(s.solve());
-        s = new Solver();
-        Lit a = new Lit(s);
-        Lit b = new Lit(s);
-        Lit c = new Lit(s);
-        Lit d = new Lit(s);
+        s = new monosat.Solver();
+        monosat.Lit a = new monosat.Lit(s);
+        monosat.Lit b = new monosat.Lit(s);
+        monosat.Lit c = new monosat.Lit(s);
+        monosat.Lit d = new monosat.Lit(s);
         s.addClause(a);
         s.addClause(a,b.not());
         s.addClause(a.not(),b,c);
         s.addClause(a.not(),b,c,d.not());
         assertTrue(s.solve());
-        ArrayList<Lit> clause = new ArrayList<Lit>(Arrays.asList(a,b.not(),c.not(),d));
+        ArrayList<monosat.Lit> clause = new ArrayList<monosat.Lit>(Arrays.asList(a,b.not(),c.not(),d));
         s.addClause(clause);
         assertTrue(s.solve());
     }
 
     @Test
     public void test_AMO() {
-        Solver solver = new Solver();
-        Lit a = new Lit(solver);
-        Lit b = new Lit(solver);
-        Lit c = new Lit(solver);
-        Lit result = Logic.xnor(a, b, c);
+        monosat.Solver solver = new monosat.Solver();
+        monosat.Lit a = new monosat.Lit(solver);
+        monosat.Lit b = new monosat.Lit(solver);
+        monosat.Lit c = new monosat.Lit(solver);
+        monosat.Lit result = monosat.Logic.xnor(a, b, c);
         assertTrue(solver.solve());
         //An empty AMO call should have no effect
         solver.assertAtMostOne();
@@ -126,31 +126,31 @@ public class SolverTest {
 
     @Test
     public void getLits() {
-        Solver s = new Solver();
+        monosat.Solver s = new monosat.Solver();
         assertEquals(s.getLits().size(),1);
-        Lit a = new Lit(s);
-        Lit b = new Lit(s);
-        Lit c = new Lit(s);
-        Lit d = new Lit(s);
+        monosat.Lit a = new monosat.Lit(s);
+        monosat.Lit b = new monosat.Lit(s);
+        monosat.Lit c = new monosat.Lit(s);
+        monosat.Lit d = new monosat.Lit(s);
         assertEquals(s.getLits().size(),5);
         s.addClause(a);
         s.addClause(a,b.not());
         s.addClause(a.not(),b,c);
         s.addClause(a.not(),b,c,d.not());
         assertTrue(s.solve());
-        ArrayList<Lit> clause = new ArrayList<Lit>(Arrays.asList(a,b.not(),c.not(),d));
+        ArrayList<monosat.Lit> clause = new ArrayList<monosat.Lit>(Arrays.asList(a,b.not(),c.not(),d));
         s.addClause(clause);
         assertTrue(s.solve());
 
         assertEquals(s.getLits().size(),5); //plus 1, for the constant true literal
-        for(Lit l:s.getLits()){
+        for(monosat.Lit l:s.getLits()){
             boolean val = l.value();
         }
     }
 
     @Test
     public void version(){
-        String version  = Solver.getVersion();
+        String version  = monosat.Solver.getVersion();
         System.out.println(version);
         assertFalse(version.isEmpty());
     }
